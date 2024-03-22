7aa4245541f0910143561ec651eeab830314442b
Arslan0900 <arslankhan2029@gmail.com>
2024-03-22 21:03:46 +0500

    add feature Text en to ar

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,40 +1,43 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const Feature = () => {
+  const [t, i18n] = useTranslation("global");
+  const currentLanguage = i18n.resolvedLanguage;
   return (
     <>
     <div className='Feature'>
     <div className='feature_text' >
-      <div className="text1">Discover the Heartbeat of your rewards</div>
-      <div className="text2"><h1>Features</h1></div>
+      <div className="text1">{t("feature.body")}</div>
+      <div className="text2"><h1>{t("feature.heading")}</h1></div>
     </div>
     <div className='feature_box'>
       <div className="box">
         <div className="box_svg"><img src="./Assets/pointex-database-svg.gif" alt="" /></div>
         <div className="box_text">
-          <div className="box_text_h"><h4>All-in-one Hub</h4></div>
-          <div className="box_text_p"><p>Consolidate your points from multiple partners here, providing a comprehensive 360-degree view.</p></div>
+          <div className="box_text_h"><h4>{t("feature.heading_box1")}</h4></div>
+          <div className="box_text_p"><p>{t("feature.description_box1")}</p></div>
         </div>
       </div>
       <div className="box">
         <div className="box_svg"><img src="./Assets/pointex-ringing-Phone-svg.gif" alt="" /></div>
         <div className="box_text">
-          <div className="box_text_h"><h4>Your Phone Number is the Key</h4></div>
-          <div className="box_text_p"><p>With your mobile number, you can perform a range of actions: register, transfer, and earn!</p></div>
+          <div className="box_text_h"><h4>{t("feature.heading_box2")}</h4></div>
+          <div className="box_text_p"><p>{t("feature.description_box2")}</p></div>
         </div>
       </div>
       <div className="box">
         <div className="box_svg"><img src="./Assets/coin-svg.gif" alt="" /></div>
         <div className="box_text">
-          <div className="box_text_h"><h4>Earn from All & transfer to Any</h4></div>
-          <div className="box_text_p"><p>Effortlessly earn rewards from all registered partners and redeem them with any of them.</p></div>
+          <div className="box_text_h"><h4>{t("feature.heading_box3")}</h4></div>
+          <div className="box_text_p"><p>{t("feature.description_box3")}</p></div>
         </div>
       </div>
       <div className="box">
         <div className="box_svg"><img src="./Assets/pointex-gift-svg.gif" alt="" /></div>
         <div className="box_text">
-          <div className="box_text_h"><h4>Voucher Friendly</h4></div>
-          <div className="box_text_p"><p>Generate and utilize valuable vouchers that are accepted by a wide range of partners.</p></div>
+          <div className="box_text_h"><h4>{t("feature.heading_box4")}</h4></div>
+          <div className="box_text_p"><p>{t("feature.description_box4")}</p></div>
         </div>
       </div>
     </div>
