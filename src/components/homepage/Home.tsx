2a29ec9e7129844abe4e39f038df60c9010d4fb7
Arslan0900 <arslankhan2029@gmail.com>
2024-03-22 08:21:58 +0500

    make home page and nabar mobile responsive

--- src/components/homepage/Home.tsx
+++ src/components/homepage/Home.tsx
@@ -9,25 +9,6 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      {currentLanguage === "ar" ? (
-        <>
-        <div className="arabic_right_box">
-        <div className="arabic_text_btn_container">
-          <div className="arabic_text_box">
-            <h2>{t("home.body")}</h2>
-          </div>
-          <div className="arabic_button_box">
-            <p>{t("home.btn")}</p>
-          </div>
-        </div>
-      </div>
-      <div className="arabic_left_box">
-        <div className="arabic_phone_img"><img src="./Assets/pointex-phone.png" alt="arabic Phone "/></div>
-        <div className="arabic_card_img" ><img src="./Assets/pointex-card-arabic.png" alt="arabic card "/></div>
-      </div>
-      </>
-      ) : (
-        <>
         <div className="left-box">
         <div className="text_btn_container">
           <div className="text-box">
@@ -40,11 +21,8 @@ const Home = () => {
       </div>
       <div className="right-box">
         <div className="phone_img"><img src="./Assets/pointex-phone.png" alt="Phone "/></div>
-        <div className="card_img" ><img src="./Assets/pointex-card.png" alt="card "/></div>
+        <div className="card_img" ><img src={`${currentLanguage === "en" ? ("./Assets/pointex-card.png"):("./Assets/pointex-card-arabic.png")}`} alt="card "/></div>
       </div>
-      </>
-      )}
-      
     </div>
   );
 };
